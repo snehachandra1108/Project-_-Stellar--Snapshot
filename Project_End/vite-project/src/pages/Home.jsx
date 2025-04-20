4079480b7de5dde08ba416897b62ac0078b5ee68
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-20 16:09:02 +0530

    Home.jsx - part of navbar

---
+++ Project_End/vite-project/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Welcome to StellarSnapshot 🌌</h1>
+      <p className="mt-2">Enter a date to explore the stars from that night.</p>
+    </div>
+  );
+};
+
+export default Home;
