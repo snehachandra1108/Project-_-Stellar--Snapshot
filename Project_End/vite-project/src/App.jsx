94e6ede4508514f4ae12b492206e4847e6250c90
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-20 16:39:15 +0530

    Update App.jsx

--- Project_End/vite-project/src/App.jsx
+++ Project_End/vite-project/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import StarAlignment from './pages/StarAlignment.jsx';
+import AboutUniverse from './pages/AboutUniverse.jsx';
+import Contact from './pages/Contact.jsx';
+import Navbar from './components/Navbar.jsx';
 
-function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-    )
-}
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/star-alignment" element={<StarAlignment />} />
+        <Route path="/about" element={<AboutUniverse />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
