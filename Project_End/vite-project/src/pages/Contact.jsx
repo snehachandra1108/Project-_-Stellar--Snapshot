7128ec5b251011e13ea9861b20f3100f87a56db0
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-20 16:11:11 +0530

    Create Contact.jsx

---
+++ Project_End/vite-project/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">About the App 👩‍🚀</h1>
+      <p className="mt-2">Made with love by Sneha 💫</p>
+    </div>
+  );
+};
+
+export default Contact;
