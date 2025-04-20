b1cc1c05ddc2b41a69e497d5b5951a8f03f2cc84
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-20 16:09:56 +0530

    StarAlignment.jsx - part of Navbar

---
+++ Project_End/vite-project/src/pages/StarAlignment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StarAlignment = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Star Alignment Page</h1>
+      <p className="mt-2">You’ll see the stars of the selected date here.</p>
+    </div>
+  );
+};
+
+export default StarAlignment;
