17fecdb366ddda138d48cfd2140fd630f949a82e
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-20 16:10:42 +0530

    AboutUniverse.jsx - part of Navbar

---
+++ Project_End/vite-project/src/pages/AboutUniverse.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUniverse = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">About the Universe 🌠</h1>
+      <p className="mt-2">Some cool facts about stars, planets, and galaxies!</p>
+    </div>
+  );
+};
+
+export default AboutUniverse;
