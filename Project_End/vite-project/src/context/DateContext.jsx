8a1f740193fd7eb0c2b75afbf1a666024f493169
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-21 19:34:28 +0530

    Update DateContext.jsx - with comments

--- Project_End/vite-project/src/context/DateContext.jsx
+++ Project_End/vite-project/src/context/DateContext.jsx
@@ -1,14 +1,17 @@
 import React, { createContext, useState } from 'react';
 
+// Create a Context for sharing the selected date across components
 export const DateContext = createContext();
 
 export const DateProvider = ({ children }) => {
+
+   // State to store the currently selected date
   const [selectedDate, setSelectedDate] = useState('');
 
   return (
+    // Make the selectedDate and its updater function available
     <DateContext.Provider value={{ selectedDate, setSelectedDate }}>
       {children}
     </DateContext.Provider>
   );
 };
-
