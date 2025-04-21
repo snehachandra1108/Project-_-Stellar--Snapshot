e456241dd257d3dc421362269121f676688e3e7f
Sneha Chandra <snehachandra1108@gmail.com>
2025-04-21 19:38:01 +0530

    Update Contact.jsx - (just the text had changed)

--- Project_End/vite-project/src/pages/Contact.jsx
+++ Project_End/vite-project/src/pages/Contact.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="min-h-screen bg-black text-white p-6">
-      <h2 className="text-4xl font-bold text-center mb-6">📬 About This App</h2>
+    <div className="min-h-screen w-screen bg-black text-white px-6 py-10 overflow-x-hidden">
+      <h2 className="text-4xl font-bold text-center mb-6">About This App</h2>
 
-      <div className="max-w-2xl mx-auto text-lg space-y-4">
+      <div className="max-w-6xl mx-auto text-lg space-y-4">
         <p>
-          <span className="text-yellow-400 font-semibold">StellarSnapshot</span> is a React-powered app that lets you view the stars, planets, and celestial information for any date you choose.
+          <span className="text-yellow-400 font-semibold">StellarSnapshot</span> is a React-powered app that lets you view the stars, planets, and celestial information for any date you choose in the past.
         </p>
 
         <p>
@@ -15,12 +15,12 @@ const Contact = () => {
         </p>
 
         <p>
-          Built using modern web tools: <strong>React</strong>, <strong>Tailwind CSS</strong>, <strong>React Router</strong>, and <strong>Context API</strong>.
+          Built using: <strong>React</strong>, <strong>Tailwind CSS</strong>, <strong>React Router</strong>, and <strong>Context API</strong>.
         </p>
 
         <hr className="border-gray-600 my-4" />
 
-        <p>👩‍💻 Developed by: <span className="text-yellow-300 font-medium">Sneha Chandra</span></p>
+        <p>Developed by: <span className="text-yellow-300 font-medium">Sneha Chandra</span></p>
         <p>
           🔗 GitHub:{" "}
           <a
@@ -29,7 +29,8 @@ const Contact = () => {
             rel="noopener noreferrer"
             className="text-blue-400 underline hover:text-blue-300"
           >
-            GitHub
+            
+            Link
           </a>
         </p>
         <p>
@@ -40,7 +41,7 @@ const Contact = () => {
             rel="noopener noreferrer"
             className="text-blue-400 underline hover:text-blue-300"
           >
-            LinkedIn
+            Link
           </a>
         </p>
       </div>
